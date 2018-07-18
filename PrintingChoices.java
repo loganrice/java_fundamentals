24fb133ab900018219508fa1bf965ce26043c2de
Logan Rice <loganrice@Logans-Air-2.attlocal.net>
2018-07-18 12:20:50 -0700

    Ex 3 PrintingChoices

---
+++ PrintingChoices.java
@@ -0,0 +1,17 @@
+public class PrintingChoices {
+  public static void main(String[] args) {
+    System.out.println( "Alpha" );
+    System.out.println( "Bravo" );
+    System.out.println( "Charlie" );
+    System.out.println( "Delta" );
+    System.out.println( "" );
+
+    System.out.print( "Echo" );
+    System.out.print( "Fox Trot" );
+
+    System.out.println( "Golf" );
+    System.out.print( "Hotel" );
+    System.out.print( " " );
+    System.out.println( "India" );
+  }
+}
